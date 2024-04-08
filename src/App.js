3d80a9aea0fde9f3a71abe9ef787470a92a0a11e
urvish01 <urvishrupareliya@gmail.com>
2024-04-08 18:22:23 +0530

    projec-done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [color, setColor] = useState('olive');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-full h-screen duration-200' style={{ backgroundColor: color }}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 ">
+        <div className=" fixed flex flex-wrap gap-3 justify-center shadow-lg bg-white px-3 py-2 rounded-3xl ">
+          <button onClick={() => setColor('red')} className=' outline-none px-4 py-1 rounded-full shadow-lg text-white ' style={{ backgroundColor: 'red' }}> Red</button>
+
+          <button onClick={() => setColor('black')} className=' outline-none px-4 py-1 rounded-full shadow-lg text-white ' style={{ backgroundColor: 'black' }}> Black </button>
+
+          <button onClick={() => setColor('pink')} className=' outline-none px-4 py-1 rounded-full shadow-lg text-white ' style={{ backgroundColor: 'pink' }}> Pink</button>
+          
+          <button onClick={() => setColor('green')} className=' outline-none px-4 py-1 rounded-full shadow-lg text-white ' style={{ backgroundColor: 'green' }}> Green</button>
+
+          <button onClick={() => setColor('yellow')} className=' outline-none px-4 py-1 rounded-full shadow-lg text-white ' style={{ backgroundColor: 'yellow' }}> Yellow</button>
+
+          <button onClick={() => setColor('white')} className=' outline-none px-4 py-1 rounded-full shadow-lg text-black ' style={{ backgroundColor: 'white' }}> White</button>
+
+
+        </div>
+      </div>
     </div>
   );
 }
